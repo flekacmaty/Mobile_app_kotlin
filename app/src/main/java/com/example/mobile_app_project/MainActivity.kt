9387e9db2da5caecefdcea03f8e31bd252fef476
flekacmaty <flekac.matyas@seznam.cz>
2025-11-27 17:11:51 +0100

    Added splashscreen

--- app/src/main/java/com/example/mobile_app_project/MainActivity.kt
+++ app/src/main/java/com/example/mobile_app_project/MainActivity.kt
@@ -3,14 +3,26 @@ package com.example.mobile_app_project
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
 import com.example.mobile_app_project.ui.navigation.AppNavGraph
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Install Android 12+ SplashScreen
+        val splash = installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        // Optional: keep splash for a short duration
+        lifecycleScope.launch {
+            delay(600)
+        }
+
         setContent {
             val navController = rememberNavController()
             Surface(color = MaterialTheme.colorScheme.background) {
